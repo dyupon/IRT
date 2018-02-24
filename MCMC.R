cd0056ccc2c04957cee25f2a23b95139903968af
Veronika Ponizova <ponizova.v@gmail.com>
2018-02-24 19:27:23 +0300

    Acceptance function fixed (unstable)

--- MCMC.R
+++ MCMC.R
@@ -14,7 +14,7 @@ rasch.modeling <- function(persons, items) {
 }
 npersons <- 100
 nitems <- 20
-items <- rnorm(nitems, 0, 2)
+items <- rnorm(nitems, 0, 1)
 persons <- rnorm(npersons)
 rmdata <- rasch.modeling(persons, items)
 X <- rmdata$data
@@ -34,7 +34,6 @@ data.like <- function(param, data){
   return(like)
 }
 
-# Ability log-prior
 theta.prior <- function(mean = 0, sd = 1, val){
   mu <- mean # prior mean
   s <- sd # prior standard deviation
@@ -43,7 +42,6 @@ theta.prior <- function(mean = 0, sd = 1, val){
   return(prior)
 }
 
-# Difficulty log-prior
 b.prior <- function(mean = 0, sd = 1, val){
   mu <- mean
   s <- sd
@@ -68,15 +66,18 @@ acceptance <- function(proposal, chain.theta, chain.beta, X, mode) {
   nitems <- length(chain.beta)
   npersons <- length(chain.theta)
   if (mode == 0) {
+    n <- npersons
     P.proposal <- outer(chain.beta, proposal, probability)
     cond.prob.current <- exp(colSums(log(P) * X + log(1 - P)*(1 - X)))
     cond.prob.proposal <- exp(colSums(log(P.proposal) * X + log(1 - P.proposal)*(1 - X)))
   } else {
+    n <- nitems
     P.proposal <- outer(proposal, chain.theta, probability)
     cond.prob.current <- exp(rowSums(log(P) * X + log(1 - P)*(1 - X)))
     cond.prob.proposal <- exp(rowSums(log(P.proposal) * X + log(1 - P.proposal)*(1 - X)))
   }
-  return(cond.prob.proposal*theta.prior(val = proposal)/(cond.prob.current*b.prior(val = chain.beta)))
+  ratio <- cond.prob.proposal*theta.prior(val = proposal)/(cond.prob.current*b.prior(val = chain.beta))
+  return(pmin(ratio, rep(1,n)))
 }
 
 
@@ -91,10 +92,10 @@ rasch.gs <- function(startvalue.theta, startvalue.beta, iterations, data, nitems
   for (i in 2:iterations) {
     t.proposal <- prop.theta(chain.theta[i - 1,]) # sample a new value for theta
     b.proposal <- prop.beta(chain.beta[i - 1,])
-    chain.theta[i,] <- ifelse(acceptance(t.proposal, chain.theta[i - 1,], chain.beta[i - 1,], X, 0) >= 1, 
+    chain.theta[i,] <- ifelse(acceptance(t.proposal, chain.theta[i - 1,], chain.beta[i - 1,], X, 0) < runif(1), 
            t.proposal,
            chain.theta[i - 1,])
-    chain.beta[i,] <- ifelse(acceptance(b.proposal, chain.theta[i - 1,], chain.beta[i - 1,], X, 1) >= 1,
+    chain.beta[i,] <- ifelse(acceptance(b.proposal, chain.theta[i - 1,], chain.beta[i - 1,], X, 1) < runif(1),
            b.proposal,
            chain.beta[i - 1,])
   }
@@ -104,17 +105,18 @@ rasch.gs <- function(startvalue.theta, startvalue.beta, iterations, data, nitems
 
 startvalue.theta <- rep(0.1, npersons)
 startvalue.beta <- rep(0.1, nitems)
+iterations <- 5000
 
-mc <- rasch.gs(startvalue.theta, startvalue.beta, 5000, X, nitems, npersons)
+mc <- rasch.gs(startvalue.theta, startvalue.beta, iterations, X, nitems, npersons)
 
 par(mfrow = c(1,1))
 burnIn <- 25
-hist(mc$chain.theta[1:1000, 1],nclass = 30, main = "Posterior of theta_1")
-abline(v = mean(mc$chain.theta[1:1000,1]), col = "red")
-hist(mc$chain.beta[1:1000,1],nclass = 30, main = "Posterior of beta_1")
-abline(v = mean(mc$chain.beta[1:1000,1]), col = "red")
-plot(mc$chain.theta[1:1000,1], type = "l", main = "Chain values of theta_1" )
-plot(mc$chain.beta[1:1000,1], type = "l",  main = "Chain values of beta_1" )
+hist(mc$chain.theta[, 1],nclass = 30, main = "Posterior of theta_1")
+abline(v = mean(mc$chain.theta[,1]), col = "red")
+hist(mc$chain.beta[,1],nclass = 30, main = "Posterior of beta_1")
+abline(v = mean(mc$chain.beta[,1]), col = "red")
+plot(as.vector(mc$chain.theta[,1]), type = "l", main = "Chain values of theta_1" )
+plot(as.vector(mc$chain.beta[,1]), type = "l",  main = "Chain values of beta_1" )
 print(mc$chain.theta[1:burnIn, 1])
 print(mc$chain.beta[1:burnIn, 1])
 fitdistr(mc$chain.theta[burnIn,], "normal")
