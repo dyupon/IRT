3d21a3b92c897591a30b04bffd60ba96b6c682f9
Veronika Ponizova <ponizova.v@gmail.com>
2018-04-15 14:18:19 +0300

    first non-functional version

--- EM.R
+++ EM.R
@@ -1,6 +1,7 @@
+library(MASS)
 library(ggplot2)
-set.seed(62)
 library(grid)
+set.seed(62)
 
 layout <- function(x, y) viewport(layout.pos.row = x, layout.pos.col = y)
 
@@ -42,23 +43,24 @@ rasch.modelling <- function(persons, items) {
   R
 }
 
-npersons <- 10000
+npersons <- 100
 nitems <- 20
 ndiscrete <- 2
 discrete.space <- c(2, 5) 
-
-sampleDist = function(n) { 
+prob <- c(0.4, 0.6)
+sampleDist = function(n, discrete.space) { 
   sample(x = discrete.space, n, replace = T, prob = c(0.4, 0.6))
 } 
 
-persons <- sampleDist(npersons)
+persons <- sampleDist(npersons, discrete.space)
 items <- rnorm(nitems, mean = 3, sd = 1)
 rmdata <- rasch.modelling(persons, items)
 Y <- t(rmdata)
 
 
 #APPROXIMATIONS
-thetas.s <- rep(1/ndiscrete, ndiscrete) 
+thetas.s <- unique(runif(4*ndiscrete))[1:ndiscrete]
+thetas.s <- thetas.s/sum(thetas.s)
 betas.s <- rep(3, nitems) 
 
 #UITLS
@@ -93,20 +95,11 @@ EM <- function(betas.s, thetas.s, discrete.space, npersons, nitems, ndiscrete, Y
   # E STEP  
   while (abs(old.likelihood - new.likelihood) > eps) {
     iter_count <- iter_count + 1
-    P <- outer(betas.s, discrete.space, probability)
-    # сворачиваем по j
-    prod_j <- exp(colSums(log(P)[, rep(1:ndiscrete, npersons)] * Y[, rep(1:npersons, each = ndiscrete)] + log(1 - P)[, rep(1:ndiscrete, npersons)] * (1 - Y)[, rep(1:npersons, each = ndiscrete)]))
-    # получили матрицу
-    dim(prod_j) <- c(ndiscrete, npersons)
-    # домножили на пи
-    print(length(prod_j))
-    prod_j_pi <- prod_j * thetas.s
-    # свернули по k -- нормировали вдоль столбцов на сумму по столбцам
-    prod_j_pi_k <- t(t(prod_j_pi)/rowSums(t(prod_j_pi)))
-    # свернули по N
-    n <- rowSums(prod_j_pi_k) #equation 13
-    r <- rowSums(prod_j_pi_k[rep(1:ndiscrete, nitems), ] * Y[rep(1:nitems, each = ndiscrete), ])
-    r <- matrix(r,nrow = nitems, ncol = ndiscrete, byrow = TRUE) #equation 14
+    persons.s <- sampleDist(npersons, thetas.s)
+    Y.s <- t(rasch.modelling(persons.s, betas.s))
+    n <- sapply(thetas.s,  function(x) sum(persons.s == x))
+    r <- sapply(thetas.s, function(x) rowSums(Y.s[,which(persons.s == x)]))
+
   
     # M STEP
     thetas.s <- n/npersons
@@ -115,17 +108,24 @@ EM <- function(betas.s, thetas.s, discrete.space, npersons, nitems, ndiscrete, Y
                  NULL, 
                  n, r,
                  method = "Nelder-Mead", 
-                 control = c(maxit = 100, reltol = 1e-8, alpha = 1.0, beta = 0.5, gamma = 2.0, fnscale = -1))
+                 control = c(maxit = 100, reltol = 1e-8, alpha = 1.0, 
+                             beta = 0.5, gamma = 2.0, fnscale = -1)
+                 )
     betas.s <- opt$par
     old.likelihood <- new.likelihood
     new.likelihood <- opt$value
-    if (iter_count %% 10 == 0) {
+    #if (iter_count %% 10 == 0) {
       print("iteration")
       print(iter_count)
       print(betas.s)
       print(thetas.s)
       print(new.likelihood)
-    }
+    #}
+      p1 <- ggplot(as.data.frame(cumsum(thetas.s)), aes(cumsum(thetas.s))) + stat_ecdf(geom = "step")
+      p2 <- ggplot(as.data.frame(cumsum(prob)), aes(cumsum(prob))) + stat_ecdf(geom = "step")
+      p3 <- ggplot(as.data.frame(betas.s), aes(betas.s)) + geom_density()
+      p4 <- ggplot(as.data.frame(items), aes(items)) + geom_density()
+      arrange(p1,p2,p3,p4, ncol = 2)
   }
   "amount of iterations done:"
   iter_count
@@ -141,10 +141,5 @@ EM <- function(betas.s, thetas.s, discrete.space, npersons, nitems, ndiscrete, Y
   c(0.4, 0.6)
   "estimated theta:"
   thetas.s
-  p1 <- ggplot(as.data.frame(thetas.s), aes(thetas.s)) + stat_ecdf(geom = "step")
-  p2 <- ggplot(as.data.frame(c(0.4, 0.2, 0.4)), aes(c(0.4, 0.2, 0.4))) + stat_ecdf(geom = "step")
-  p3 <- ggplot(as.data.frame(betas.s), aes(betas.s)) + geom_density()
-  p4 <- ggplot(as.data.frame(items), aes(items)) + geom_density()
-  arrange(p1,p2,p3,p4, ncol = 2)
   return(list("beta" = betas.s, "theta" = thetas.s))
 }
