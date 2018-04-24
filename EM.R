fee86f1efc0a0615f83b4d837749c0798ae868c4
Veronika Ponizova <ponizova.v@gmail.com>
2018-04-24 16:28:51 +0300

    smth

--- EM.R
+++ EM.R
@@ -47,13 +47,13 @@ npersons <- 100
 nitems <- 20
 ndiscrete <- 2
 discrete.space <- c(2, 5) 
-prob <- c(0.4, 0.6)
+prob <- c(0.1, 0.9)
 sampleDist = function(n, prob) { 
   sample(x = discrete.space, n, replace = T, prob)
 } 
 
 persons <- sampleDist(npersons, prob)
-items <- rnorm(nitems, mean = 3, sd = 1)
+items <- rnorm(nitems, mean = 0, sd = 1)
 rmdata <- rasch.modelling(persons, items)
 Y <- t(rmdata)
 
@@ -68,7 +68,9 @@ probability <- function(beta, theta) {
   return(1 / (1 + exp(beta - theta)))
 }
 
-log.likelihood <- function(x, n, r) { # equation 16
+regularized.log.likelihood <- function(x, n, r, iter_count) { # equation 16
+  k <- 0.4
+  lambda <- 1
   P <- outer(x, discrete.space, probability)
   bet <- rep(0, nitems) 
   for (j in (1:nitems)) {
@@ -76,24 +78,30 @@ log.likelihood <- function(x, n, r) { # equation 16
     for (k in (1:ndiscrete)) {
       bet.k[k] <- r[j,k]*log(P)[j,k] + (n[k] - r[j,k])*log(1 - P)[j,k]
     }
-    bet[j] <- sum(bet.k)
+    bet[j] <- sum(bet.k) 
   }
-  res <- sum(bet)
+  res <- sum(bet) - lambda*k^iter_count*(sum(x^2))
+  #res <- sum(bet) - lambda*k^iter_count*(1/nitems)*sum(dnorm(x, log = T)*dnorm(x))
   return(res)
 }
-eps <- 10^-6
 
+
+eps <- 10^-6
+trajectory.thetas <- data.frame(x = thetas.s[1], y = thetas.s[2])
+trajectory.betas <- data.frame(x = betas.s[1], y = betas.s[10], z = betas.s[20])
 #P -- matrix w/ probabilities JxK (nitems x ndisrete)
 #Y -- response matrix JxN  (nitems x npersons)
 #pi_s -- vector of length K (ndiscrete)
 #r_s -- matrix JxK (nitems x ndisrete)
 
 EM <- function(betas.s, thetas.s, discrete.space, npersons, nitems, ndiscrete, Y) { 
+  k <- 0.4
+  lambda <- 1
   old.likelihood <- 1
   new.likelihood <- 0
   iter_count <- 0
   # E STEP  
-  for (iter_count in 1:10000) {
+  for (iter_count in (1:1000)) {
     persons.s <- sampleDist(npersons, thetas.s)
     Y.s <- t(rasch.modelling(persons.s, betas.s))
     n <- sapply(discrete.space,  function(x) sum(persons.s == x))
@@ -102,33 +110,30 @@ EM <- function(betas.s, thetas.s, discrete.space, npersons, nitems, ndiscrete, Y
         Y.s[, which(persons.s == x)]
       } else {
         rowSums(Y.s[, which(persons.s == x)])
-        }
-      })
+      }
+    })
     # M STEP
     thetas.s <- n/npersons
     opt <- optim(betas.s, 
-                 log.likelihood, 
+                 regularized.log.likelihood, 
                  NULL, 
-                 n, r,
+                 n, r, iter_count,
                  method = "Nelder-Mead", 
                  control = c(maxit = 100, reltol = 1e-8, alpha = 1.0, 
                              beta = 0.5, gamma = 2.0, fnscale = -1)
-                 )
+    )
     betas.s <- opt$par
+    if (thetas.s[1] != 1) {
+      trajectory.thetas <- rbind(trajectory.thetas, thetas.s)
+      trajectory.betas <- rbind(trajectory.betas, c(betas.s[1], betas.s[10], betas.s[20]))
+    }
     old.likelihood <- new.likelihood
     new.likelihood <- opt$value
-    #if (iter_count %% 10 == 0) {
-      print("iteration")
-      print(iter_count)
-      print(betas.s)
-      print(thetas.s)
-      print(new.likelihood)
-    #}
-      # p1 <- ggplot(as.data.frame(cumsum(thetas.s)), aes(cumsum(thetas.s))) + stat_ecdf(geom = "step")
-      # p2 <- ggplot(as.data.frame(cumsum(prob)), aes(cumsum(prob))) + stat_ecdf(geom = "step")
-      # p3 <- ggplot(as.data.frame(betas.s), aes(betas.s)) + geom_density()
-      # p4 <- ggplot(as.data.frame(items), aes(items)) + geom_density()
-      # arrange(p1,p2,p3,p4, ncol = 2)
+    print("iteration")
+    print(iter_count)
+    print(betas.s)
+    print(thetas.s)
+    print(new.likelihood)
   }
   "amount of iterations done:"
   iter_count
@@ -144,5 +149,11 @@ EM <- function(betas.s, thetas.s, discrete.space, npersons, nitems, ndiscrete, Y
   c(0.4, 0.6)
   "estimated theta:"
   thetas.s
-  return(list("beta" = betas.s, "theta" = thetas.s))
+  return(list("beta" = betas.s, "theta" = thetas.s, "trajectory.thetas" = trajectory.thetas, "trajectory.betas" = trajectory.betas)) 
 }
+result1 <- EM(betas.s, thetas.s, discrete.space, npersons, nitems, ndiscrete, Y)
+plot(as.vector(result1$trajectory.betas$x), type = "l",  main = "Chain values of beta_1")
+plot(as.vector(result1$trajectory.betas$y), type = "l",  main = "Chain values of beta_10")
+plot(as.vector(result1$trajectory.betas$z), type = "l",  main = "Chain values of beta_20")
+plot(as.vector(result1$trajectory.thetas$x), type = "l",  main = "Chain values of pi_1")
+plot(as.vector(result1$trajectory.thetas$y), type = "l",  main = "Chain values of pi_2")
